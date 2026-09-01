6d9c55d164a2c8dd98ef4d4c53e6e702e9d495df
stlsmoko <skidstoleave@gmail.com>
2026-09-02 08:59:10 +1000

    Bump to 1.0.47 for smoother Cutout playback

--- app.config.ts
+++ app.config.ts
@@ -43,7 +43,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.0.46",
+  version: "1.0.47",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
@@ -64,7 +64,7 @@ const config: ExpoConfig = {
     },
     predictiveBackGestureEnabled: false,
     package: androidPackage,
-    versionCode: 47,
+    versionCode: 48,
     permissions: ["POST_NOTIFICATIONS"],
     intentFilters: [
       {
