ac13a36ef82dbc46df8f9a7f9ea6a133bbf029bd
stlsmoko <skidstoleave@gmail.com>
2026-09-03 01:05:48 +1000

    Bump to 1.0.50 so reel audio plays naturally

--- app.config.ts
+++ app.config.ts
@@ -32,7 +32,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.0.49",
+  version: "1.0.50",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
@@ -53,7 +53,7 @@ const config: ExpoConfig = {
     },
     predictiveBackGestureEnabled: false,
     package: androidPackage,
-    versionCode: 50,
+    versionCode: 51,
     permissions: ["POST_NOTIFICATIONS"],
     intentFilters: [
       {
