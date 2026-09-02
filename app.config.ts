031f7bf9d6f4adebca05ff6f075bbe378a20b41a
stlsmoko <skidstoleave@gmail.com>
2026-09-03 06:31:32 +1000

    Bump to 1.0.58 so long Cutout takes stay in sync

--- app.config.ts
+++ app.config.ts
@@ -32,7 +32,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.0.57",
+  version: "1.0.58",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
@@ -53,7 +53,7 @@ const config: ExpoConfig = {
     },
     predictiveBackGestureEnabled: false,
     package: androidPackage,
-    versionCode: 58,
+    versionCode: 59,
     permissions: ["POST_NOTIFICATIONS"],
     intentFilters: [
       {
