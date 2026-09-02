608f0d2a12df8b2e6ffa7d62f7da90075e1806f0
stlsmoko <skidstoleave@gmail.com>
2026-09-03 04:48:26 +1000

    Bump to 1.0.56 after merging zip slider and camera fixes

--- app.config.ts
+++ app.config.ts
@@ -32,7 +32,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.0.55",
+  version: "1.0.56",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
@@ -53,7 +53,7 @@ const config: ExpoConfig = {
     },
     predictiveBackGestureEnabled: false,
     package: androidPackage,
-    versionCode: 56,
+    versionCode: 57,
     permissions: ["POST_NOTIFICATIONS"],
     intentFilters: [
       {
